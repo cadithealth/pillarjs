1da1f98e3935b52fd172597d134390832e7523f1
David Yeung <quxbaz@gmail.com>
2014-03-26 16:38:33 -0400

    Added doc comments and .run function

--- module.js
+++ module.js
@@ -1,18 +1,197 @@
 /*
-  module.js
+  Module
+  Synchronous dependency resolver.
 
-  Synchronous dependency loader.
+  * What Is it?
 
-  TODO: Give it a trendy name. E.g, Tangler, Lasso, Curkit
-  TODO: <description>
-  TODO: Write unit tests.
-  TODO: What about circular dependencies?
-  TODO: What about server-side stuff?
-  TODO: What about other libraries (?:create adapters)?
-  TODO: Allow global/default options.
-  TODO: In register, disallow any tag names that can't be used as in function params.
-  TODO: Allow namespace needs(). E.g,
+    Module is a javascript dependency resolver. It helps you structure
+    your app as modules.
 
+    Basically, you define your app as a bunch of encapsulated
+    modules.
+
+    Each module is a function that lists its dependencies to other
+    modules.
+
+    You have one module called "main" that kicks everything off. That's
+    pretty much all there is to it.
+
+  * Why Does This Exist?
+
+    RequireJS is a fine tool, but I had some issues with it. In
+    production, you usually want to compress your assets into a single
+    file and serve it as such. In development, you want to serve your
+    assets separately. In dev mode with RequireJS, you perform
+    asynchronous requests to the server to get modules, but this
+    breaks parity with how you get assets in production (ie, in prod,
+    all your files are in one big glob, so you don't need to hit the
+    server).
+
+    Usually, this makes no difference, but there are cases where it
+    does. For example, if you're building an Ember app and define your
+    routes in different modules - the app will fail if you try to
+    navigate to a route before it's module file has been loaded. In
+    production mode, however, it would work.
+
+    In development and production, Module performs loading the same
+    way; synchronously. It makes no difference if your files are
+    served separately, concatenated, or minified.
+
+  * Basic Usage
+
+    To define a module:
+      module.register(moduleName, function() {...})
+
+    To include a module:
+      module.register('foobar', function(moduleA, moduleB) {...})
+    OR
+      needs('moduleA', 'moduleB')
+
+  * Where To Include Files
+
+    Suggestions on where to include your asset files (ie, <head>,
+    <body>) are only guidelines. Do what's best for your app.
+
+    - Include this file in the <head> tag so it loads quickly.
+    - Include your module definition files in the <body>.
+    - You modules need to be defined before they can be loaded, so
+      include your main module after your module definitions.
+
+  ** Working With Third Party Libraries
+
+     I prefer to simply leave third-party libraries as globals, but if
+     you want to be very strictly modular, you can define adapters
+     like so:
+
+       module.register('$', function() {
+         var jQuery = jQuery;
+         window.$ = null;  // Remove global references to jQuery.
+         window.jQuery = null;
+         return jQuery;
+       });
+
+       module.register('Backbone', function() {
+         var Backbone = Backbone;
+         window.Backbone = null;
+         return Backbone;
+       });
+
+    You might want to package all your adapters into a single
+    module like this:
+
+       module.register('adaptors', function() {
+
+         module.register('$', function() {
+           var jQuery = jQuery;
+           window.$ = null;  // Remove global references to jQuery.
+           window.jQuery = null;
+           return jQuery;
+         });
+
+
+         module.register('Backbone', function() {
+           var Backbone = Backbone;
+           window.Backbone = null;
+           return Backbone;
+         });
+
+         return module.needs(['$', 'Backbone'])
+
+       });
+
+    Then from your main module, you can do this:
+
+      module.register('main', function() {
+        runs('adaptors');  // Same as needs, but suppresses return value.
+      });
+
+  * Issues
+    - Circular dependencies are an issue. Module fails if it
+      detects if it detects one. For example, this is critically bad:
+
+        module.register('foo', function(bar) {...});
+        module.register('bar', function(foo) {...});
+
+  * Guidelines For A Good Dependency Resolver
+    - Doesn't get in the way of:
+      - [X] Debugging
+      - [X] Minification
+      - [X] Gzip compression
+      - [X] File concatentation
+      - [X] Caching
+    - [ ] File load order doesn't matter.
+    - [X] Supports fast development/reloads.
+    - [X] Production and development mode is the same.
+
+  * Differences with RequireJS
+
+    - The biggest difference is that this library doesn't do
+      asynchronous loading in dev mode. Module is not a module loader,
+      it is a dependency resolver whereas RequireJS is a module loader
+      AND a dependency resolver. This actually makes it more
+      comparable to Almond than RequireJS.
+
+    - RequireJS executes code within a define block as soon as the
+      file is loaded. Module waits until a module is needed before it
+      executes its define block.
+
+    - Doesn't come packaged with a directory traversal or file
+      minification/concatenation tool akin r.js
+
+    - Defined modules must be given names so they can be
+      referenced. The downside to this is that names must be managed,
+      but module referencing is more flexible.
+
+    - With Module, you can define a module anywhere and as many as you
+      want within a single file. You can also list and get
+      dependencies anywhere you want. For example, this is perfectly valid code:
+
+        // utils.js
+
+        module.register('domUtils', function() {});
+        module.register('strUtils', function(depA, depB) {
+
+          // This has the same effect of listing "depC" in the parameters.
+          var depC = needs('depC');
+
+          // More includes.
+          var moreDeps = needs(['app', 'math', 'trig']);
+
+          for (var i=0; i < 4; i++) {
+
+            // Registering modules dynamically.
+            module.register('module' + i, function() {
+              return i;
+            }));
+
+          }
+
+        });
+
+    - Dependencies can be listed in function parameters similar to
+      AngularJS. Example:
+
+        // RequireJS
+        define(['app/app', 'utils/dom'], function(app, domUtils), {...});
+
+        // Module
+        module.register('main', function(app, domUtils) {...});
+
+  * Similarities with RequireJS
+
+    - Both require an entry point file. RequireJS requires a main.js
+      file. Module simply requires you to register a module named
+      "main".
+
+  * TODO
+    - Give it a trendy name. Eg, Tangler, Lasso, Curkit,
+      standoff. "Module" is too generic and probably taken.
+    - Write unit tests.
+    - What about circular dependencies?
+    - Detect circular dependencies and throw errors.
+    - Allow global/default options.
+    - In register, disallow any tag names that can't be used as in function params.
+    - Allow namespace needs(). Eg,
 
     Allow separator option (default=/).
     needs:
@@ -157,6 +336,15 @@
       return moduleRets;
     }
 
+  };
+
+  // Alias for needs(), but suppresses return value. You can always
+  // use needs() instead, but calling this instead makes it clear that
+  // you care only about the module's definition code, not whatever
+  // (if anything) the module returns.
+  window.runs = function() {
+    needs.apply(null, arguments);
+    return null;
   };
 
   window.module = loader;
